96c4dd101007f50d7dd164dd9de7d245cd87d006
Denis Ghiuzan <denis.ghiuzan@endava.com>
2020-03-18 17:23:25 +0200

    added mostenire ierarhica

--- ModelDesignFirst/SelfReferenceCodeFirst/TestProgram.cs
+++ ModelDesignFirst/SelfReferenceCodeFirst/TestProgram.cs
@@ -10,22 +10,76 @@ namespace ModelDesignFirst.SelfReferenceCodeFirst
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Test self reference");
-            TestSelfReference();
+            //Console.WriteLine("Test self reference");
+            //TestSelfReference();
 
-            Console.WriteLine("Test self Vertical splitting");
-            TestVerticalSplitting();
+            //Console.WriteLine("Test self Vertical splitting");
+            //TestVerticalSplitting();
 
-            Console.WriteLine("Test splitting");
-            TestSplitting();
+            //Console.WriteLine("Test splitting");
+            //TestSplitting();
 
-            Console.WriteLine("Test mostenire tip");
-            TestMostenireTip();
+            //Console.WriteLine("Test mostenire tip");
+            //TestMostenireTip();
+
+            Console.WriteLine("Test mostenire ierarhica");
+            TestMostenireIerarhica();
 
             Console.ReadKey();
         }
 
-        public static void TestMostenireTip()
+        private static void TestMostenireIerarhica()
+        {
+            using (var context = new EmployeeContext())
+            {
+                var fte = new FullTimeEmployee
+                {
+                    FirstName = "Jane",
+                    LastName =
+                        "Doe",
+                    Salary = 71500M
+                };
+                context.Employees.Add(fte);
+                fte = new FullTimeEmployee
+                {
+                    FirstName = "John",
+                    LastName = "Smith",
+                    Salary = 62500M
+                };
+                context.Employees.Add(fte);
+                var hourly = new HourlyEmployee
+                {
+                    FirstName = "Tom",
+                    LastName =
+                        "Jones",
+                    Wage = 8.75M
+                };
+                context.Employees.Add(hourly);
+                context.SaveChanges();
+
+                Console.WriteLine("--- All Employees ---");
+                foreach (var emp in context.Employees)
+                {
+                    bool fullTime = emp is HourlyEmployee ? false : true;
+                    Console.WriteLine("{0} {1} ({2})", emp.FirstName, emp.LastName,
+                        fullTime ? "Full Time" : "Hourly");
+                }
+                Console.WriteLine("--- Full Time ---");
+                foreach (var fullTimeEmployee in context.Employees.OfType<FullTimeEmployee>())
+                {
+                    Console.WriteLine("{0} {1}", fullTimeEmployee.FirstName, fullTimeEmployee.LastName);
+                }
+                Console.WriteLine("--- Hourly ---");
+                foreach (var hourlyEmployee in context.Employees.OfType<HourlyEmployee>())
+                {
+                    Console.WriteLine("{0} {1}", hourlyEmployee.FirstName,
+                        hourlyEmployee.LastName);
+                }
+
+            }
+        }
+
+        private static void TestMostenireTip()
         {
             using (var context = new BusinessContext())
             {
